7d714b340f6870e211ab20b57254db3c4cc8c878
StefanKarneback <stefan.ewb@telia.com>
2023-04-30 18:04:17 +0200

    updates from integratedsoalrcooking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LandingScreen from "./screens/LandingScreen2";
+import FirelessCooker from "./screens/FirelessCooker";
+import SolarCookerScreen from "./screens/SolarCooker";
+import React from "react";
+import ProductsScreen from "./screens/Products";
+import ContactScreen from "./screens/ContactScreen";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
+import FuelSavingStove from "./screens/FuelSavingStove";
+import AdditionalInfo from "./screens/ReadMore";
+import VendorTable from "./components/VendorsTable";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Hamburger hamPosition">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Router>
+          <Routes>
+            <Route
+              path="/Integratedsolarcooking"
+              element={<LandingScreen isOpen={isOpen} />}
+            />
+            <Route
+              path="/FirelessCooker"
+              element={<FirelessCooker isOpen={isOpen} />}
+            />
+            <Route
+              path="/SolarCooker"
+              element={<SolarCookerScreen isOpen={isOpen} />}
+            />
+            <Route
+              path="/FuelSavingStove"
+              element={<FuelSavingStove isOpen={isOpen} />}
+            />
+            <Route
+              path="/Products"
+              element={<ProductsScreen isOpen={isOpen} />}
+            />
+            <Route
+              path="/Contact"
+              element={<ContactScreen isOpen={isOpen} />}
+            />
+            <Route
+              path="/AdditionalInfo"
+              element={<AdditionalInfo isOpen={isOpen} />}
+            />
+            <Route path="/Vendors" element={<VendorTable isOpen={isOpen} />} />
+            <Route
+              path="/*"
+              element={<Navigate to="/Integratedsolarcooking" replace />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
-
-export default App;
